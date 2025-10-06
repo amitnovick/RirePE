f5326f79df6b1b0a3daad24684b0c57042b97821
Amit Novick <axnovick@gmail.com>
2025-10-06 19:58:21 +0300

    Fix BuildEvent.bat path for Simple library
      The Simple.lib is built to Share/Simple/Release/ so the copy
      command needs to use ../Release/ not Release/

--- Share/Simple/BuildEvent.bat
+++ Share/Simple/BuildEvent.bat
@@ -1,4 +1,5 @@
 @echo off
 set PROJECTNAME=%1
-REM Copy the built library from the project's Release folder to the parent Simple directory
-xcopy "Release\%PROJECTNAME%.lib" ".." /Y
+REM Copy the built library from the solution's Release folder to the parent Simple directory
+REM The lib is at $(SolutionDir)Release\Simple.lib, which is ../Release/ relative to the project
+xcopy "..\Release\%PROJECTNAME%.lib" ".." /Y
