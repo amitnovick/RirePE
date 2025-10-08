2df04689fb4fd450194709bbebbc4caca12e87f3
Amit Novick <axnovick@gmail.com>
2025-10-08 18:05:42 +0300

    testing SEND functionality

--- RirePE/FormatGUI.cpp
+++ RirePE/FormatGUI.cpp
@@ -29,13 +29,25 @@ bool FVOnCreate(Alice &fv) {
 	SendDlgItemMessageW(fv.GetMainHWND(), FV_EDIT_INFO, EM_SETLIMITTEXT, (WPARAM)0x100000, 0);
 	//fv.ReadOnly(FV_EDIT_INFO);
 	fv.Button(FV_SEND, L"Send", (FV_WIDTH - 3) - 210, (FV_HEIGHT - 3) - 20, 100);
-	fv.ChangeState(FV_SEND, false);
 	fv.Button(FV_RECV, L"Recv", (FV_WIDTH - 3) - 100, (FV_HEIGHT - 3) - 20, 100);
 	return true;
 }
 
 bool FVOnCommand(Alice &fv, int nIDDlgItem) {
 	if (nIDDlgItem == FV_SEND) {
+		Alice &main_gui = GetMainGUI();
+		std::wstring wText = fv.GetText(FV_EDIT_INFO);
+		RScript rs(wText, GetHeaderSize());
+
+		if (!rs.Parse()) {
+			wText = L"RScript Error!";
+			//main_gui.SetText(EDIT_PACKET_SEND, wText);
+			return false;
+		}
+
+		wText = rs.getRaw();
+		//main_gui.SetText(EDIT_PACKET_SEND, wText);
+		PacketSender(main_gui, SENDPACKET, wText);
 		return true;
 	}
 	if (nIDDlgItem == FV_RECV) {
