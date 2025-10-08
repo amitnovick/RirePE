aee934d788f058488dce0f69c1afcb98d1895007
Amit Novick <axnovick@gmail.com>
2025-10-09 01:33:46 +0300

    phase 7 refactoring

--- Share/Hook/BuildEvent64.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-set PROJECTNAME=%1
-REM Hook64 outputs to $(SolutionDir)Release\ which is one level up from this project
-xcopy "..\Release\%PROJECTNAME%.lib" ".." /Y
--- Share/Simple/BuildEvent64.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-set PROJECTNAME=%1
-REM Simple64 outputs to $(SolutionDir)Release\ which is one level up from this project
-xcopy "..\Release\%PROJECTNAME%.lib" ".." /Y
